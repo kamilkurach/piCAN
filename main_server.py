4fd30f58924d143a1c16591ea7d7643494bbb327
yacotaco <kamilkurach@protonmail.com>
2021-03-20 17:29:19 +0100

    Add: exception handling

--- main_server.py
+++ main_server.py
@@ -6,27 +6,39 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_piCAN():
-    r = requests.get(ip.MAIN_PAGE)
-    result = "Hello, from piCAN! [STATUS_CODE: {0}]".format(r.status_code)
-    return result
+    try:
+        r = requests.get(ip.MAIN_PAGE)
+        result = "Hello, from piCAN! [STATUS_CODE: {0}]".format(r.status_code)
+        return result
+    except requests.exceptions.ConnectionError as e:
+        raise SystemExit(e)
 
 @app.route('/on')
 def piCAN_on():
-    r = requests.get(ip.ON)
-    result = "led on [STATUS_CODE: {0}]".format(r.status_code)
-    return result
+    try:
+        r = requests.get(ip.ON)
+        result = "led on [STATUS_CODE: {0}]".format(r.status_code)
+        return result
+    except requests.exceptions.ConnectionError as e:
+        raise SystemExit(e)
 
 @app.route('/off')
 def piCAN_off():
-    r = requests.get(ip.OFF)
-    result = "led off [STATUS_CODE: {0}]".format(r.status_code)
-    return result
+    try:
+        r = requests.get(ip.OFF)
+        result = "led off [STATUS_CODE: {0}]".format(r.status_code)
+        return result
+    except requests.exceptions.ConnectionError as e:
+        raise SystemExit(e)
 
 @app.route('/blink')
 def piCAN_blink():
-    r = requests.get(ip.BLINK)
-    result = "led blink [STATUS_CODE: {0}]".format(r.status_code)
-    return result
+    try:
+        r = requests.get(ip.BLINK)
+        result = "led blink [STATUS_CODE: {0}]".format(r.status_code)
+        return result
+    except requests.exceptions.ConnectionError as e:
+        raise SystemExit(e)
 
 if __name__ == '__main__':
     app.run()
