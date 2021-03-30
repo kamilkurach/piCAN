0c8c7365776bdba5b69ae93d69fb83241db263b6
yacotaco <kamilkurach@protonmail.com>
2021-03-30 09:11:52 +0200

    Split helper functions for server and pi server

--- helper_functions.py
+++ helper_functions.py
@@ -1,19 +1,7 @@
 # helper functions
-import cv2
-import base64
+
 from time import sleep 
 import RPi.GPIO as GPIO
-
-def generate_stream():
-    cap = cv2.VideoCapture(0)
-    if cap.isOpened() == True:
-        while True: 
-            ret, frame = cap.read() 
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            retval, buffer = cv2.imencode('.jpg', image)
-            img_str = base64.b64encode(image)
-            out = "data:image/jpeg;base64,{0}".format(img_str)
-            yield out
 
 class Motor:
     def __init__(self):
